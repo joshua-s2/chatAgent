c1e2a4bbcc9e2a63687a8c4d706e197489b1816f
JoshuaS2 <joshuajesutobi@gmail.com>
2025-09-21 05:53:47 +0100

    minor updates

--- types/other/utilsPlugins.ts
+++
@@ -1,7 +0,0 @@
-export interface filterPluginTypes {
-  capitalize: (value: string) => string;
-  obfuscateEmail: (value: string, totalLength: number) => string;
-  darkenColor: (hex: string, percentage: number) => string;
-  generateRandomColor: (letter?: string) => Array<string>;
-  extractFirstLetter: (name: string) => string;
-}
