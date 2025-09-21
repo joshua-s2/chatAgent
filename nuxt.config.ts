87375e4419604a66ff053eb2acab3e33498e3797
JoshuaS2 <joshuajesutobi@gmail.com>
2025-09-21 16:59:33 +0100

    added a little bit of seo for open graph preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,49 @@ export default defineNuxtConfig({
       apiBase: process.env.API_BASE,
     },
   },
+
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+    head: {
+      title: "Chat agent - A lightweight chat agent inspired by Siena AI",
+      meta: [
+        {
+          name: "description",
+          content: "A lightweight chat agent inspired by Siena AI",
+        },
+        { name: "twitter:card", content: "summary_large_image" },
+        {
+          name: "twitter:title",
+          content:
+            "Chat agent Spectrum - A lightweight chat agent inspired by Siena AI",
+        },
+        {
+          name: "twitter:description",
+          content: "A lightweight chat agent inspired by Siena AI",
+        },
+        {
+          name: "twitter:image",
+          content: "",
+        },
+        { property: "og:type", content: "website" },
+        {
+          property: "og:title",
+          content: "Chat agent - A lightweight chat agent inspired by Siena AI",
+        },
+        {
+          property: "og:description",
+          content: "A lightweight chat agent inspired by Siena AI",
+        },
+        { property: "og:url", content: "https://chat-agent-joshua.vercel.app" },
+        {
+          property: "og:image",
+          content: "",
+        },
+        { name: "theme-color", content: "#0a0a0a" },
+      ],
+    },
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
